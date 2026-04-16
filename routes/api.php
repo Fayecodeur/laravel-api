29411501280e7a8ab675574bcb8fcb538d72cdd1
Demba Faye <fayecodeur99@gmail.com>
2026-04-16 16:36:30 +0000

    Creation du route pour les api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::apiResource('posts', PostController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
