3d33c4d06806ef7d6115e4c1d540b0b7ca4908ee
Demba Faye <fayecodeur99@gmail.com>
2026-04-20 13:53:31 +0000

    mise en place du service API avec Axios

---
+++ frontend/src/services/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const URL = "http://127.0.0.1:8000/api/posts";
+
+export const getPosts = async () => {
+  const response = await axios.get(URL);
+  return response.data;
+};
