8e2491a3bc917b6eefd5ef3295eaa17c18e0fcd0
Demba Faye <fayecodeur99@gmail.com>
2026-04-23 16:48:18 +0000

    Import de la navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Posts from "./pages/Posts";
+import CreatePost from "./pages/CreatePost";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <section>
-      <Posts />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/create" element={<CreatePost />} />
+      </Routes>
     </section>
   );
 }
