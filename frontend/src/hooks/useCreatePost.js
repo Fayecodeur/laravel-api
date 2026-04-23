3f3b9e1484d4bba6179ac22f66fb992f9067e6b3
Demba Faye <fayecodeur99@gmail.com>
2026-04-23 18:20:15 +0000

    Ajout du toast success

--- frontend/src/hooks/useCreatePost.js
+++ frontend/src/hooks/useCreatePost.js
@@ -1,12 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createPost } from "../services/api";
+import toast from "react-hot-toast";
 
 export const useCreatePost = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: createPost,
     onSuccess: () => {
+      toast.success("Post ajouté avec succès");
       queryClient.invalidateQueries(["post"]);
+    },
+    onError: () => {
+      toast.error("Une erreur est survenue");
     },
   });
 };
