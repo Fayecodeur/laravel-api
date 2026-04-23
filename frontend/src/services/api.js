3a27696bd80dd97b1f834c05ed9e0dd60f2f3bd4
Demba Faye <fayecodeur99@gmail.com>
2026-04-23 16:47:02 +0000

    Ajout d'un post

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,3 +6,8 @@ export const getPosts = async () => {
   const response = await axios.get(URL);
   return response.data.data;
 };
+
+export const createPost = async (data) => {
+  const response = await axios.post(URL, data);
+  return response.data;
+};
